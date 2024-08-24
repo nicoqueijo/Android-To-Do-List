87e5da0980ae80d012ea809bff9a664bd08f3a8c
Nicolas Queijo <queijonicolas@gmail.com>
2024-08-24 18:46:50 -0400

    Added ui events.

--- app/src/main/java/com/nicoqueijo/android/todolist/domain/ToDoViewModel.kt
+++ app/src/main/java/com/nicoqueijo/android/todolist/domain/ToDoViewModel.kt
@@ -1,12 +1,15 @@
 package com.nicoqueijo.android.todolist.domain
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.nicoqueijo.android.todolist.core.di.DefaultDispatcher
+import com.nicoqueijo.android.todolist.domain.model.UiEvent
 import com.nicoqueijo.android.todolist.presentation.model.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -18,4 +21,57 @@ class ToDoViewModel @Inject constructor(
     private val _uiState = MutableStateFlow(value = UiState())
     val uiState = _uiState.asStateFlow()
 
+    init {
+        /*setIsFirstLaunch()*/
+        /*setSampleToDos()*/
+        viewModelScope.launch(context = dispatcher) {
+            /*val toDos = useCases.retrieveToDosUseCase()
+            toDos.collectLatest { databaseToDos ->
+                updateToDos()
+            }*/
+        }
+    }
+
+    /**
+     * Handles various UI events and delegates them to the appropriate use cases or functions.
+     *
+     * @param event The UI event to handle.
+     */
+    fun onEvent(event: UiEvent) {
+        when (event) {
+            UiEvent.AddToDo -> {
+                TODO()
+            }
+            UiEvent.CancelDialog -> {
+                TODO()
+            }
+            UiEvent.CompleteToDo -> {
+                TODO()
+            }
+            UiEvent.ConfirmDialog -> {
+                TODO()
+            }
+            UiEvent.DeleteAllToDos -> {
+                TODO()
+            }
+            UiEvent.DeleteToDo -> {
+                TODO()
+            }
+            UiEvent.EditToDo -> {
+                TODO()
+            }
+            UiEvent.ReorderToDos -> {
+                TODO()
+            }
+            UiEvent.RestoreToDo -> {
+                TODO()
+            }
+            UiEvent.SaveToDo -> {
+                TODO()
+            }
+            UiEvent.ToggleOffIsFirstLaunch -> {
+                TODO()
+            }
+        }
+    }
 }
--- app/src/main/java/com/nicoqueijo/android/todolist/domain/model/UiEvent.kt
+++ app/src/main/java/com/nicoqueijo/android/todolist/domain/model/UiEvent.kt
@@ -1,12 +1,12 @@
 package com.nicoqueijo.android.todolist.domain.model
 
 sealed interface UiEvent {
-    data object AddToDo: UiEvent
-    data object EditToDo: UiEvent
-    data object SaveToDo: UiEvent
-    data object CompleteToDo: UiEvent
-    data object DeleteAllToDos: UiEvent
-    data object DeleteToDo: UiEvent
+    data object AddToDo: UiEvent // Invokes bottom sheet with title & description fields empty & Save button on bottom right corner that is disabled unless the title is not empty or blank.
+    data object EditToDo: UiEvent // Invokes bottom sheet with title & description fields of selected ToDo
+    data object SaveToDo: UiEvent // Dismiss bottom sheet and save ToDo to the db
+    data object CompleteToDo: UiEvent // Save the clicked ToDo to db with a flipped isCompleted field
+    data object DeleteAllToDos: UiEvent // Delete all ToDos from db
+    data object DeleteToDo: UiEvent // Delete selected ToDo from db
     data object RestoreToDo: UiEvent
     data object ReorderToDos: UiEvent
     data object ConfirmDialog : UiEvent
