d033e3030dea40d3592a0b216bd6926c7b4c18a9
Nicolas Queijo <queijonicolas@gmail.com>
2024-08-25 09:50:35 -0400

    Fixed bottom sheet colors and style.

--- app/src/main/java/com/nicoqueijo/android/todolist/presentation/composables/EmptyListIndicator.kt
+++ app/src/main/java/com/nicoqueijo/android/todolist/presentation/composables/EmptyListIndicator.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.nicoqueijo.android.todolist.R
 import com.nicoqueijo.android.todolist.presentation.theme.ToDoListTheme
 import com.nicoqueijo.android.todolist.presentation.util.DarkLightPreviews
@@ -54,12 +55,14 @@ fun EmptyListIndicator(
                 text = stringResource(id = R.string.empty_list_title),
                 color = MaterialTheme.colorScheme.primary,
                 style = MaterialTheme.typography.titleLarge,
+                fontSize = 24.sp,
                 textAlign = TextAlign.Center
             )
             Text(
                 text = stringResource(id = R.string.add_todo_item_instruction),
                 color = MaterialTheme.colorScheme.primary,
                 style = MaterialTheme.typography.titleMedium,
+                fontSize = 20.sp,
                 textAlign = TextAlign.Center
             )
         }
--- app/src/main/java/com/nicoqueijo/android/todolist/presentation/composables/ToDoBottomSheetItem.kt
+++ app/src/main/java/com/nicoqueijo/android/todolist/presentation/composables/ToDoBottomSheetItem.kt
@@ -1,25 +1,36 @@
 package com.nicoqueijo.android.todolist.presentation.composables
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.nicoqueijo.android.todolist.R
 import com.nicoqueijo.android.todolist.domain.model.ToDo
 import com.nicoqueijo.android.todolist.presentation.theme.ToDoListTheme
 import com.nicoqueijo.android.todolist.presentation.util.DarkLightPreviews
+import com.nicoqueijo.android.todolist.presentation.util.S
+import com.nicoqueijo.android.todolist.presentation.util.XXS
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -29,51 +40,107 @@ fun ToDoBottomSheetItem(
     onSave: ((ToDo) -> Unit)? = null,
     onDismiss: (() -> Unit)? = null,
 ) {
-    val sheetState = rememberModalBottomSheetState(
-        skipPartiallyExpanded = true
-    )
-    val scope = rememberCoroutineScope()
-
     ModalBottomSheet(
-        onDismissRequest = {
-            onDismiss?.invoke()
-        },
-        sheetState = sheetState
+        modifier = modifier,
+        onDismissRequest = { onDismiss?.invoke() },
+        containerColor = MaterialTheme.colorScheme.surfaceVariant,
+        contentColor = MaterialTheme.colorScheme.surfaceVariant
     ) {
-        Column(
+        SheetContent(
+            state = state,
+            onSave = onSave,
+        )
+    }
+}
+
+@Composable
+fun SheetContent(
+    modifier: Modifier = Modifier,
+    state: ToDo?,
+    onSave: ((ToDo) -> Unit)? = null,
+) {
+    val title = remember { mutableStateOf(state?.title ?: "") }
+    val description = remember { mutableStateOf(state?.description ?: "") }
+    val focusRequester = remember { FocusRequester() }
+
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+    }
+    Column(
+        modifier = modifier
+            .background(color = MaterialTheme.colorScheme.surfaceVariant)
+            .fillMaxWidth()
+            .padding(S)
+    ) {
+        TextField(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(16.dp)
+                .padding(bottom = XXS)
+                .focusRequester(focusRequester),
+            value = title.value,
+            onValueChange = { newText ->
+                title.value = newText
+            },
+            label = {
+                Text(
+                    text = stringResource(R.string.label_title)
+                )
+            },
+            colors = TextFieldDefaults.colors(
+                focusedTextColor = MaterialTheme.colorScheme.primary,
+                unfocusedTextColor = MaterialTheme.colorScheme.primary,
+                unfocusedContainerColor = Color.Transparent,
+                focusedContainerColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent
+            )
+        )
+        TextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = XXS),
+            value = description.value,
+            onValueChange = { newText ->
+                description.value = newText
+            },
+            label = {
+                Text(
+                    text = stringResource(R.string.label_description)
+                )
+            },
+            colors = TextFieldDefaults.colors(
+                focusedTextColor = MaterialTheme.colorScheme.primary,
+                unfocusedTextColor = MaterialTheme.colorScheme.primary,
+                unfocusedContainerColor = Color.Transparent,
+                focusedContainerColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent
+            )
+        )
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = S),
+            contentAlignment = Alignment.BottomEnd
         ) {
-            TextField(
-                value = state?.title ?: "",
-                onValueChange = { },
-                label = { Text("Title") },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 8.dp)
-            )
+            Button(
+                onClick = {
 
-            TextField(
-                value = state?.description ?: "",
-                onValueChange = { },
-                label = { Text("Description") },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 8.dp)
-            )
-
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 16.dp),
-                contentAlignment = Alignment.BottomEnd
+                },
+                enabled = title.value.isNotBlank(),
+                colors = ButtonDefaults.buttonColors(
+                    disabledContainerColor = MaterialTheme.colorScheme.tertiary,
+                    disabledContentColor = MaterialTheme.colorScheme.tertiaryContainer.copy(
+                        alpha = 0.5f
+                    )
+                )
             ) {
-                Button(
-                    onClick = { }
-                ) {
-                    Text("Save", fontSize = 16.sp, fontWeight = FontWeight.Bold)
-                }
+                Text(
+                    text = stringResource(R.string.label_save),
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold
+                )
             }
         }
     }
@@ -81,13 +148,27 @@ fun ToDoBottomSheetItem(
 
 @DarkLightPreviews
 @Composable
-fun ToDoBottomSheetItemPreview() {
+fun SheetContentPreview() {
     ToDoListTheme {
-        ToDoBottomSheetItem(
+        SheetContent(
             state = ToDo(
                 position = 1,
                 title = "Exercise",
                 description = "Go for a 30-minute run in the park and complete a 15-minute stretching session.",
+            )
+        )
+    }
+}
+
+@DarkLightPreviews
+@Composable
+fun SheetContentEmptyPreview() {
+    ToDoListTheme {
+        SheetContent(
+            state = ToDo(
+                position = 1,
+                title = "",
+                description = "",
             )
         )
     }
